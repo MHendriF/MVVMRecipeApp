f5c763d6fa9a4dc70d30946516fec1f122542ade
MHendriF <hendrifebriansyah28@gmail.com>
2021-02-10 17:35:53 +0700

    Navigation fragment

--- app/src/main/java/com/mhendrif/mvvmrecipeapp/RecipeFragment.kt
+++ app/src/main/java/com/mhendrif/mvvmrecipeapp/RecipeFragment.kt
@@ -5,9 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.ComposeView
@@ -15,27 +13,17 @@ import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
 import androidx.fragment.app.Fragment
-import androidx.navigation.findNavController
 
-class RecipeListFragment : Fragment() {
+class RecipeFragment: Fragment() {
 
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return ComposeView(requireContext()).apply {
             setContent {
                 Column(modifier = Modifier.padding(16.dp)) {
                     Text(
-                        text = "RecipeList",
-                        style = TextStyle(fontSize = TextUnit.Companion.Sp(21))
+                        text = "Recipe Fragment",
+                        style = TextStyle(fontSize = TextUnit.Sp(21))
                     )
-                    Spacer(modifier = Modifier.padding(10.dp))
-                    Button(onClick = { findNavController().navigate(R.id.viewRecipe) }) {
-                        Text(text = "T0 Recipe Fragment")
-                    }
-
                 }
             }
         }
