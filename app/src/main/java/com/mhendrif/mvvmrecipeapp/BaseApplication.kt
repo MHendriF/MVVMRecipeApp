e9e2480e31b8cea681c705a219427ff61b28aff5
MHendriF <hendrifebriansyah28@gmail.com>
2021-02-11 16:50:34 +0700

    Setup dependency injection

---
+++ app/src/main/java/com/mhendrif/mvvmrecipeapp/BaseApplication.kt
@@ -0,0 +1,9 @@
+package com.mhendrif.mvvmrecipeapp
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class BaseApplication : Application(){
+
+}
---
+++ app/src/main/java/com/mhendrif/mvvmrecipeapp/di/AppModule.kt
@@ -0,0 +1,27 @@
+package com.mhendrif.mvvmrecipeapp.di
+
+import android.content.Context
+import com.mhendrif.mvvmrecipeapp.BaseApplication
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ApplicationComponent
+import dagger.hilt.android.qualifiers.ApplicationContext
+import javax.inject.Singleton
+
+@Module
+@InstallIn(ApplicationComponent::class)
+object AppModule {
+
+    @Singleton
+    @Provides
+    fun provideApplication(@ApplicationContext app: Context): BaseApplication {
+        return app as BaseApplication
+    }
+
+    @Singleton
+    @Provides
+    fun provideRandomString(): String{
+        return "Hey look a random string!!!!!GNKGNDFK"
+    }
+}
