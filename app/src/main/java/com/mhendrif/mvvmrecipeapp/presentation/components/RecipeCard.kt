d24f626b2c5d4243f96dbb898a3a9f5f02b9eee0
MHendriF <hendrifebriansyah28@gmail.com>
2021-02-12 22:29:04 +0700

    Asynchronously Load Images

--- app/src/main/java/com/mhendrif/mvvmrecipeapp/presentation/components/RecipeCard.kt
+++ app/src/main/java/com/mhendrif/mvvmrecipeapp/presentation/components/RecipeCard.kt
@@ -9,11 +9,14 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.asImageBitmap
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.imageResource
 import androidx.compose.ui.unit.dp
 import com.mhendrif.mvvmrecipeapp.domain.model.Recipe
 import com.mhendrif.mvvmrecipeapp.R
+import com.mhendrif.mvvmrecipeapp.util.DEFAULT_RECIPE_IMAGE
+import com.mhendrif.mvvmrecipeapp.util.loadPicture
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 
 @ExperimentalCoroutinesApi
@@ -35,13 +38,16 @@ fun RecipeCard(
     ) {
         Column {
             recipe.featuredImage?.let { url ->
-                Image(
-                    bitmap = imageResource(id = R.drawable.empty_plate),
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .preferredHeight(225.dp),
-                    contentScale = ContentScale.Crop,
-                )
+                val image = loadPicture(url = url, defaultImage = DEFAULT_RECIPE_IMAGE).value
+                image?.let { img ->
+                    Image(
+                        bitmap = img.asImageBitmap(),
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .preferredHeight(225.dp),
+                        contentScale = ContentScale.Crop,
+                    )
+                }
             }
             recipe.title?.let { title ->
                 Row(
