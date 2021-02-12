37ae9ec549e184ab2198d729aed95cf36a1b6333
MHendriF <hendrifebriansyah28@gmail.com>
2021-02-12 23:13:10 +0700

    Horizontal Scrollable Row

--- app/src/main/java/com/mhendrif/mvvmrecipeapp/presentation/ui/recipe_list/RecipeListFragment.kt
+++ app/src/main/java/com/mhendrif/mvvmrecipeapp/presentation/ui/recipe_list/RecipeListFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.compose.foundation.ScrollableRow
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -14,6 +15,7 @@ import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.input.ImeAction
@@ -21,6 +23,7 @@ import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import com.mhendrif.mvvmrecipeapp.presentation.components.FoodCategoryChip
 import com.mhendrif.mvvmrecipeapp.presentation.components.RecipeCard
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.ExperimentalCoroutinesApi
@@ -46,37 +49,50 @@ class RecipeListFragment : Fragment() {
                     Surface(
                         modifier = Modifier
                             .fillMaxWidth(),
-                        color = MaterialTheme.colors.primary,
+                        color = Color.White,
                         elevation = 8.dp,
                     ) {
-                        Row(modifier = Modifier.fillMaxWidth()) {
-                            TextField(
-                                modifier = Modifier
-                                    .fillMaxWidth(.9f)
-                                    .padding(8.dp),
-                                value = query,
-                                onValueChange = {
-                                    viewModel.onQueryChanged(it)
-                                },
-                                label = {
-                                    Text(text = "Search")
-                                },
-                                keyboardOptions = KeyboardOptions(
-                                    keyboardType = KeyboardType.Text,
-                                    imeAction = ImeAction.Done,
-                                ),
-                                leadingIcon = {
-                                    Icon(Icons.Filled.Search)
-                                },
-                                onImeActionPerformed = { action, softKeyboardController ->
-                                    if (action == ImeAction.Done) {
-                                        viewModel.newSearch(query)
-                                        softKeyboardController?.hideSoftwareKeyboard()
-                                    }
-                                },
-                                textStyle = TextStyle(color = MaterialTheme.colors.onSurface),
-                                backgroundColor = MaterialTheme.colors.surface
-                            )
+                        Column {
+                            Row(modifier = Modifier.fillMaxWidth()) {
+                                TextField(
+                                    modifier = Modifier
+                                        .fillMaxWidth(.9f)
+                                        .padding(8.dp),
+                                    value = query,
+                                    onValueChange = {
+                                        viewModel.onQueryChanged(it)
+                                    },
+                                    label = {
+                                        Text(text = "Search")
+                                    },
+                                    keyboardOptions = KeyboardOptions(
+                                        keyboardType = KeyboardType.Text,
+                                        imeAction = ImeAction.Search,
+                                    ),
+                                    leadingIcon = {
+                                        Icon(Icons.Filled.Search)
+                                    },
+                                    onImeActionPerformed = { action, softKeyboardController ->
+                                        if (action == ImeAction.Done) {
+                                            viewModel.newSearch(query)
+                                            softKeyboardController?.hideSoftwareKeyboard()
+                                        }
+                                    },
+                                    textStyle = TextStyle(color = MaterialTheme.colors.onSurface),
+                                    backgroundColor = MaterialTheme.colors.surface
+                                )
+                            }
+                            ScrollableRow(modifier = Modifier.fillMaxWidth()) {
+                                for (category in getAllFoodCategories()) {
+                                    FoodCategoryChip(
+                                        category = category.value,
+                                        onExecuteSearch = {
+                                            viewModel.onQueryChanged(it)
+                                            viewModel.newSearch(it)
+                                        },
+                                    )
+                                }
+                            }
                         }
                     }
 
