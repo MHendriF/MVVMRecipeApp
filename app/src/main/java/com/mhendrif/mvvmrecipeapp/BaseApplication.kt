41cf0011f065d8f39630a7ec5e07de0342bf36cc
MHendriF <hendrifebriansyah28@gmail.com>
2021-02-17 15:29:34 +0700

    Dark Theme and Light ThemeDark Theme and Light Theme

--- app/src/main/java/com/mhendrif/mvvmrecipeapp/BaseApplication.kt
+++ app/src/main/java/com/mhendrif/mvvmrecipeapp/BaseApplication.kt
@@ -1,9 +1,16 @@
 package com.mhendrif.mvvmrecipeapp
 
 import android.app.Application
+import androidx.compose.runtime.mutableStateOf
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
 class BaseApplication : Application(){
 
+    // Should be save in datastore or cache
+    val isDark = mutableStateOf(false)
+
+    fun toggleLightTheme() {
+        isDark.value = !isDark.value
+    }
 }
--- app/src/main/java/com/mhendrif/mvvmrecipeapp/presentation/ui/recipe_list/RecipeListFragment.kt
+++ app/src/main/java/com/mhendrif/mvvmrecipeapp/presentation/ui/recipe_list/RecipeListFragment.kt
@@ -4,19 +4,27 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.MaterialTheme
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.unit.dp
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import com.mhendrif.mvvmrecipeapp.BaseApplication
 import com.mhendrif.mvvmrecipeapp.presentation.components.*
+import com.mhendrif.mvvmrecipeapp.presentation.theme.AppTheme
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import javax.inject.Inject
 
 @AndroidEntryPoint
 class RecipeListFragment : Fragment() {
+
+    @Inject
+    lateinit var application: BaseApplication
 
     private val viewModel: RecipeListViewModel by viewModels()
 
@@ -29,38 +37,47 @@ class RecipeListFragment : Fragment() {
 
         return ComposeView(requireContext()).apply {
             setContent {
-                val recipes = viewModel.recipes.value
-                val query = viewModel.query.value
-                val selectedCategory = viewModel.selectedCategory.value
-                val categoryScrollPosition = viewModel.categoryScrollPosition
-                val loading = viewModel.loading.value
 
-                Column {
+                AppTheme(darkTheme = application.isDark.value) {
+                    val recipes = viewModel.recipes.value
+                    val query = viewModel.query.value
+                    val selectedCategory = viewModel.selectedCategory.value
+                    val categoryScrollPosition = viewModel.categoryScrollPosition
+                    val loading = viewModel.loading.value
 
-                    SearchAppBar(
-                            query = query,
-                            onQueryChanged = viewModel::onQueryChanged,
-                            onExecuteSearch = viewModel::newSearch,
-                            categories = getAllFoodCategories(),
-                            selectedCategory = selectedCategory,
-                            onSelectedCategoryChanged = viewModel::onSelectedCategoryChanged,
-                            scrollPosition = categoryScrollPosition,
-                            onChangeScrollPosition = viewModel::onChangeCategoryScrollPosition,
-                    )
+                    Column {
+                        SearchAppBar(
+                                query = query,
+                                onQueryChanged = viewModel::onQueryChanged,
+                                onExecuteSearch = viewModel::newSearch,
+                                categories = getAllFoodCategories(),
+                                selectedCategory = selectedCategory,
+                                onSelectedCategoryChanged = viewModel::onSelectedCategoryChanged,
+                                scrollPosition = categoryScrollPosition,
+                                onChangeScrollPosition = viewModel::onChangeCategoryScrollPosition,
+                                onToggleTheme = {
+                                    application.toggleLightTheme()
+                                }
+                        )
 
-                    Box(modifier = Modifier.fillMaxSize()) {
-                        if (loading){
-                            LoadingRecipeListShimmer(imageHeight = 250.dp)
-                        }else{
-                            LazyColumn {
-                                itemsIndexed(
-                                        items = recipes
-                                ) { _, recipe ->
-                                    RecipeCard(recipe = recipe, onClick = {})
+                        Box(
+                            modifier = Modifier
+                                    .fillMaxSize()
+                                    .background(color = MaterialTheme.colors.background)
+                        ) {
+                            if (loading) {
+                                LoadingRecipeListShimmer(imageHeight = 250.dp)
+                            } else {
+                                LazyColumn {
+                                    itemsIndexed(
+                                            items = recipes
+                                    ) { _, recipe ->
+                                        RecipeCard(recipe = recipe, onClick = {})
+                                    }
                                 }
                             }
+                            CircularIndeterminateProgressBar(isDisplayed = loading, verticalBias = 0.3f)
                         }
-                        CircularIndeterminateProgressBar(isDisplayed = loading, verticalBias = 0.3f)
                     }
                 }
             }
